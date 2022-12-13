6f44865e2fd68f2d251c70b917e3372297be6541
Jose Torres <jota_torres@hotmaiil.es>
2022-12-13 12:45:03 +0100

    add title posts

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const titlePost = document.querySelector("#titlePost");
-
 fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => response.json())
-  .then((json) => {
-    json.forEach((titles) => {
-      let h5 = document.createElement("h5");
-      h5.classList.add("card-title");
-      titlePost.innerHTML = titles.title;
-      titlePost.appendChild(h5);
-    });
+  .then(response => response.json())
+  .then(data => {
+    const posts = data;
+    let cards = "";
+    posts.forEach(post => {
+
+        cards += `<div class= "col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${post.title}</h5>
+                        </div>
+                    </div>
+                </div>`;
+          document.querySelector('#cards').innerHTML = cards;
+      });
   });
