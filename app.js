03cc4177e5d36bf32d41b0affde65c938cd67af0
Jose Torres <jota_torres@hotmaiil.es>
2022-12-14 13:02:40 +0100

    async

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let bodyModal =document.querySelector(".modal-body");
 let userModal = document.querySelector(".userModal");
 let emailModal = document.querySelector(".emailModal");
 
+const urlPosts = "http://localhost:3000/posts";
+const urlUsers = "http://localhost:3000/users";
+
 // fetch(urlPosts)
 //   .then((response) => response.json())
 //   .then((data) => {
@@ -10,18 +13,15 @@ let emailModal = document.querySelector(".emailModal");
 //   });
 
 
-const urlPosts = "http://localhost:3000/posts";
-// const urlUsers = "https://jsonplaceholder.typicode.com/users";
 async function getData(){
-    console.log(urlPosts)
-
     const response1 = await fetch(urlPosts)
     const data1 = await response1.json()
 
-    // const response2 = await fetch(urlUsers)
-    // const data2 = await response1.json()
+    const response2 = await fetch(urlUsers)
+    const data2 = await response2.json()
 
     cardsPosts(data1);
+    // cardsPosts(data2);
 }
 getData();
 
@@ -44,11 +44,38 @@ function cardsPosts(titlesCards) {
 
 
 
+// function prueba(e){
+//   fetch(`${urlPosts}/${e.target.name}`)
+//   .then((response) => response.json())
+//   .then((modal) => {
+//     titleModal.textContent = modal.title;
+//     bodyModal.textContent = modal.body;
+//     userModal.textContent = modal.username;
+//     emailModal.textContent = modal.email;
+//   });
+// }
 function prueba(e){
+  var posts, users;
   fetch(`${urlPosts}/${e.target.name}`)
-  .then((response) => response.json())
-  .then((modal) => {
-    titleModal.textContent = modal.title;
-    bodyModal.textContent = modal.body; 
-  });
+  .then(function (response1){
+    return response1.json();
+  }).then(function (data1){
+    posts = data1;
+      titleModal.textContent = data1.title;
+      bodyModal.textContent = data1.body;
+      console.log(posts)
+      return fetch(urlUsers);
+  })
+  .then(function (response2){
+    return response2.json();
+  }).then(function (data2){
+    userModal.textContent = data2.username;
+    emailModal.textContent = data2.email ;
+    users = data2;
+    console.log(users)
+   
+}).catch(function (error) {
+
+  console.log(error);
+});
 }
