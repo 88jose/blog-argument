48713b9b6b3132e3a76ea4a453dc4fa69c916f07
Jose Torres <jota_torres@hotmaiil.es>
2022-12-14 15:03:43 +0100

    pruebas users

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-let titleModal =document.querySelector("#exampleModalLabel");
-let bodyModal =document.querySelector(".modal-body");
+let titleModal = document.querySelector("#exampleModalLabel");
+let bodyModal = document.querySelector(".modal-body");
 let userModal = document.querySelector(".userModal");
 let emailModal = document.querySelector(".emailModal");
+let usersEmails = document.querySelector('#userEmail');
 
 const urlPosts = "http://localhost:3000/posts";
-const urlUsers = "http://localhost:3000/users";
+const urlUsers = "http://localhost:3000/users/";
 
-async function getData(){
-    const response1 = await fetch(urlPosts)
-    const data1 = await response1.json()
+async function getData() {
+  const response1 = await fetch(urlPosts);
+  const data1 = await response1.json();
 
-    const response2 = await fetch(urlUsers)
-    const data2 = await response2.json()
+  const response2 = await fetch(urlUsers);
+  const data2 = await response2.json();
 
     cardsPosts(data1);
-    // cardsPosts(data2);
+    modalPosts(data2);
+
 }
 getData();
-
 
 function cardsPosts(titlesCards) {
   let cards = "";
@@ -26,35 +27,49 @@ function cardsPosts(titlesCards) {
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">${post.title}</h5>
-                            <button onclick="prueba(event)" type="button" class="btn btn-primary" data-bs-toggle="modal" name ="${post.id}" data-bs-target="#exampleModal">View post</button>
+                            <button onclick="prueba(event)" type="button" class="btn btn-primary" data-bs-toggle="modal" name="${post.id}" data-bs-target="#exampleModal">View post</button>
                         </div>
                     </div>
                 </div>`;
     document.querySelector("#cards").innerHTML = cards;
   });
 }
+function modalPosts(userEmail) {
+  let modals = "";
+  userEmail.forEach((modale) => {
+    modals += `<p class="userModal" >${modale.username}</p>
+                <br>
+               <p class="emailModal" name="${modale.email}">${modale.email}</p>`;
+              
+    document.querySelector("#userEmail").innerHTML = modals;
+  });
+}
 
-function prueba(e){
-  var posts, users;
-  fetch(`${urlPosts}/${e.target.name}`)
-  .then(function (response1){
-    return response1.json();
-  }).then(function (data1){
-    posts = data1;
+ function prueba(e) {
+  let postId =  e.target.name;
+  let posts, users;
+  fetch(`${urlPosts}/${postId}`)
+    .then(function (response1) {
+      return response1.json();
+    })
+    .then(function (data1) {
       titleModal.textContent = data1.title;
       bodyModal.textContent = data1.body;
-      console.log(posts)
-      return fetch(urlUsers);
-  })
-  .then(function (response2){
-    return response2.json();
-  }).then(function (data2){
-    userModal.textContent = data2.username;
-    emailModal.textContent = data2.email ;
-    users = data2;
-    console.log(users)
-   
-}).catch(function (error) {
-  console.log(error);
-});
+      posts = data1;
+      console.log(posts);
+      return fetch(`${urlUsers}${postId}`);
+    })
+    .then(function (response2) {
+      return response2.json();
+    })
+    .then(function (data2) {
+      users = data2;
+      usersEmails.textContent = data2.username + data2.email;
+      // usersEmails.textContent = data2.email ;
+      console.log(users);
+      
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
